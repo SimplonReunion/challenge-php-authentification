e35fda26407552cd233220b35ba70bf3b995aefd
PublicVar <dev.publicvar@gmail.com>
2016-05-23 10:28:31 +0400

    Add the password field to user table
    `password` varchar(255) NOT NULL

--- database/user.sql
+++ database/user.sql
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(255) CHARACTER SET utf8 NOT NULL,
   `firstname` varchar(255) CHARACTER SET utf8 NOT NULL,
   `lastname` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
